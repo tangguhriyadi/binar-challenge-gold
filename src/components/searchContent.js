288fd5dc72d8c6f5f5af7a70f0e5d0406d2e892a
tangguhriyadi <mtangguh97@gmail.com>
2022-07-24 17:06:32 +0700

    fix input size

--- src/components/searchContent.js
+++ src/components/searchContent.js
@@ -4,56 +4,56 @@ import { Container, Form, Row, Col, Button, Card } from "react-bootstrap";
 import { BASE_URL } from "../utils/constant";
 import Result from "./result";
 import Swal from "sweetalert2";
-import Select from 'react-select'
+import Select from "react-select";
 
 const SearchContent = () => {
   const [name, setName] = useState("");
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState("");
   const categoryList = [
     {
-      value:'2 - 4 orang',
-      label:'2 - 4 orang'
+      value: "2 - 4 orang",
+      label: "2 - 4 orang",
     },
     {
-      value:'4 - 6 orang',
-      label:'4 - 6 orang'
+      value: "4 - 6 orang",
+      label: "4 - 6 orang",
     },
     {
-      value:'6 - 8 orang',
-      label:'6 - 8 orang'
+      value: "6 - 8 orang",
+      label: "6 - 8 orang",
     },
-  ]
+  ];
   const [price, setPrice] = useState("");
   const PriceList = [
     {
-      value:'<400000',
-      label:'<Rp.400.000'
+      value: "<400000",
+      label: "<Rp.400.000",
     },
     {
-      value:'400000-600000',
-      label:'Rp.400.000 - Rp.600.000'
+      value: "400000-600000",
+      label: "Rp.400.000 - Rp.600.000",
     },
     {
-      value:'>600000',
-      label:'>Rp.600.000'
+      value: ">600000",
+      label: ">Rp.600.000",
     },
-  ]
+  ];
   const StatusList = [
     {
-      value:null,
-      label:'Disewa'
+      value: null,
+      label: "Disewa",
     },
-  ]
+  ];
   const [result, setResult] = useState(null);
   const [show, setShow] = useState(false);
-  
+
   const handleName = (e) => {
     setName(e.target.value);
   };
   const handleCategory = (e) => {
-    setCategory(e.value)
+    setCategory(e.value);
   };
- 
+
   const handlePrice = (e) => {
     setPrice(e.value);
   };
@@ -63,17 +63,15 @@ const SearchContent = () => {
   };
 
   const highlight = () => {
-    
     setShow(true);
   };
   const offHighlight = () => {
-    
     setShow(false);
   };
 
   const handleSubmit = async () => {
-    const deactive = show
-    setShow(!deactive)
+    const deactive = show;
+    setShow(!deactive);
 
     if (name.length !== 0 || category.length !== 0 || price.length !== 0) {
       await axios
@@ -138,50 +136,50 @@ const SearchContent = () => {
   };
   const styleP = {
     fontSize: "12px",
-    width: "220px",
+    width: "208px",
     fontWeight: "300",
     borderRadius: "2px",
-    paddingTop:'9px',
-    paddingBottom:'9px'
+    paddingTop: "9px",
+    paddingBottom: "9px",
   };
   const styleP2 = {
     fontSize: "12px",
     width: "220px",
     fontWeight: "300",
     borderRadius: "2px",
-    
   };
   const styleH = {
     fontSize: "14px",
     fontWeight: "700",
   };
- 
+
   const customTheme = (theme) => {
     return {
       ...theme,
-      colors:  {
+      colors: {
         ...theme.colors,
-        primary25:'#C9E7CA',
-        primary:'#5CB85F',
-      }
-    }
-  }
+        primary25: "#C9E7CA",
+        primary: "#5CB85F",
+      },
+    };
+  };
   const colorStyles = {
-    control : (styles) => ({...styles, backgroundColor:'white', width:'208px', height:'32px', borderRadius:'2px'}),
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: "white",
+      width: "208px",
+      height: "32px",
+      borderRadius: "2px",
+    }),
     option: (styles) => {
-      return {...styles}
+      return { ...styles };
     },
-    indicatorSeparator : (styles) => ({...styles, opacity:'0'}),
-    menu : (styles) => ({...styles, width:'208px'})
-  }
+    indicatorSeparator: (styles) => ({ ...styles, opacity: "0" }),
+    menu: (styles) => ({ ...styles, width: "208px" }),
+  };
   return (
     <Container className="mb-4" style={{ marginTop: "-50px" }}>
-      <div
-        id="highlight"
-        className={show ? "highlight1" : null}
-        
-        
-      >
+      <div id="highlight" className={show ? "highlight1" : null}>
         <Card className="ps-4 pe-0 pt-3 pb-4">
           {result && (
             <h5 className="mb-3 mt-2" style={styleH}>
@@ -194,7 +192,6 @@ const SearchContent = () => {
                 <Form.Label style={styleP2}>Nama Mobil</Form.Label>
                 <Form.Control
                   onChange={handleName}
-                  
                   className="selectDrop"
                   value={name}
                   type="text"
@@ -204,44 +201,61 @@ const SearchContent = () => {
               </Col>
               <Col md>
                 <Form.Label style={styleP2}>Kategori</Form.Label>
-                <div style={{fontSize:'12px', fontWeight:'300', borderRadius:'2px'}} >
-                <Select 
-                styles={colorStyles}
-                theme={customTheme}
-                options={categoryList} 
-                onChange={handleCategory} 
-                placeholder="Masukan Kapasistas" 
-                isSearchable
-                autoFocus
-                />
+                <div
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "300",
+                    borderRadius: "2px",
+                  }}
+                >
+                  <Select
+                    styles={colorStyles}
+                    theme={customTheme}
+                    options={categoryList}
+                    onChange={handleCategory}
+                    placeholder="Masukan Kapasistas"
+                    isSearchable
+                    autoFocus
+                  />
                 </div>
-               
               </Col>
               <Col md>
                 <Form.Label style={styleP2}>Harga</Form.Label>
-                <div style={{fontSize:'12px', fontWeight:'300', borderRadius:'2px'}} >
-                <Select 
-                styles={colorStyles}
-                theme={customTheme}
-                options={PriceList} 
-                onChange={handlePrice} 
-                placeholder="Masukan Harga Sewa" 
-                isSearchable
-                autoFocus
-                />
+                <div
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "300",
+                    borderRadius: "2px",
+                  }}
+                >
+                  <Select
+                    styles={colorStyles}
+                    theme={customTheme}
+                    options={PriceList}
+                    onChange={handlePrice}
+                    placeholder="Masukan Harga Sewa"
+                    isSearchable
+                    autoFocus
+                  />
                 </div>
               </Col>
               <Col md>
                 <Form.Label style={styleP2}>Status</Form.Label>
-                <div style={{fontSize:'12px', fontWeight:'300', borderRadius:'2px'}} >
-                <Select 
-                styles={colorStyles}
-                theme={customTheme}
-                options={StatusList} 
-                placeholder="Disewa" 
-                isSearchable
-                autoFocus
-                />
+                <div
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "300",
+                    borderRadius: "2px",
+                  }}
+                >
+                  <Select
+                    styles={colorStyles}
+                    theme={customTheme}
+                    options={StatusList}
+                    placeholder="Disewa"
+                    isSearchable
+                    autoFocus
+                  />
                 </div>
               </Col>
               {result ? (
@@ -260,7 +274,7 @@ const SearchContent = () => {
                     type="submit"
                     variant="none"
                     className="btn-sewa1"
-                    style={{marginTop:'30px'}}
+                    style={{ marginTop: "30px" }}
                     onClick={handleSubmit}
                   >
                     Cari Mobil
